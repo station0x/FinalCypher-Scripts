ff10909a283ccb74b07c995b9ac05e85852aa1d0
Captain Isaac A <87286968+CaptainIsaaca@users.noreply.github.com>
2022-12-09 18:10:54 +0200

    Core Logic

--- index.js
+++ index.js
@@ -1,40 +1,53 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import { execa, execaNode } from 'execa';
 import fs from 'node:fs';
+import https from 'node:https'
 import { oraPromise } from 'ora';
+import { traverse } from 'object-traversal';
+import { Octokit } from "@octokit/rest"
 
 let clientDist = '../../Releases/Client/Windows'
+let basePath = 'space/'
+const octokit = new Octokit({ auth: process.env.PAT })
+
+// Get Release Assets IDs 
+let releaseAssets = await getClientReleaseAssets()
+let assetsMapping = {}
+for (let asset in releaseAssets) {
+    assetsMapping[`${releaseAssets[asset].name}`] = releaseAssets[asset].id
+}
 
 async function writeMerkleTreeToStorage(data) {
-    // Preserve newlines, etc. - use valid JSON
-    // data = data.replace(/\\n/g, "\\n")
-    // .replace(/\\'/g, "\\'")
-    // .replace(/\\"/g, '\\"')
-    // .replace(/\\&/g, "\\&")
-    // .replace(/\\r/g, "\\r")
-    // .replace(/\\t/g, "\\t")
-    // .replace(/\\b/g, "\\b")
-    // .replace(/\\f/g, "\\f")
-    // // .replace(/\\n/g, "");
-    // // Remove non-printable and other non-valid JSON characters
-    // data = data.replace(/[\u0000-\u0019]+/g,"");
-    console.log(data)
-    fs.writeFileSync("merkle.json", JSON.stringify(data, null, 2) , 'utf-8');
-    let tree = JSON.parse(fs.readFileSync("merkle.json", "utf8"))
-    console.log(tree.name)
+    fs.writeFileSync(`${basePath}merkle.json`, data, "utf-8");
+}
 
-    // fs.writeFile("merkle.json", JSON.stringify(data, null, "\t"), (err) => {
-    //     if (err)
-    //         console.log(err);
-    //     else {
-    //         console.log("File written successfully\n");
-    //         console.log("The written has the following contents:");
-    //         // console.log(JSON.parse(fs.readFileSync("merkle.json", "utf8")));
-    //     }
-    // });
+function traverseTree(tree) {
+    let mapping = {}
+    let hashes = []
+    function getMapping({ parent, key, value, meta }) {
+        if (value.name && value.name.includes('.')) {
+            let ancestors = meta.nodePath.split('.')
+            ancestors = ancestors
+            .filter(v => {
+                if(!/^\d+$/.test(v)) return v
+            })
+            .map((v) => {
+                return v.split('@')[0]
+            })
+            .filter(val => val !== 'Windows')
+            .join('@')
+
+            mapping[`${value.hash}`] = ancestors ? ancestors + '@' + value.name : value.name
+            hashes.push(value.hash)
+        }
+    }
+    traverse(tree, getMapping, { traversalType: 'breadth-first' });
+    return {mapping, hashes}
 }
 
 async function constructMerkleTree() {
-    return execa('node_modules/.bin/folder-hash', [
+    return execa('modules/node-folder-hash/bin/folder-hash', [
         `${clientDist}`
     ]);
 }
@@ -52,25 +65,129 @@ async function flattenBuild() {
     }
 }
 
+async function getRemoteMerkleTree() {
+    return (await octokit.rest.repos.getReleaseAsset({
+        owner: 'station0x',
+        repo: 'FC-Client-Binaries',
+        asset_id: assetsMapping['merkle.json']
+    }))
+}
+
+async function getClientReleaseAssets() {
+    return (await octokit.rest.repos.getLatestRelease({
+        owner: 'station0x',
+        repo: 'FC-Client-Binaries'
+    }))
+}
+
+async function updateReleaseNotes() {
+    return (await octokit.rest.repos.updateRelease({
+        owner: 'station0x',
+        repo: 'FC-Client-Binaries',
+        release_id
+    }))
+}
+
+async function updateRelease(id) {
+    return (await octokit.rest.repos.updateReleaseAsset({
+        owner: 'station0x',
+        repo: 'FC-Client-Binaries',
+        asset_id: id,
+    }))
+}
+
+
+async function uploadReleaseAsset(id) {
+    return (await octokit.rest.repos.updateRelease({
+        owner: 'station0x',
+        repo: 'FC-Client-Binaries',
+        release_id: id,
+    }))
+}
+
+function getFlattenedName(path) {
+    return path.replaceAll('/', '@')
+}
+
+
+/**
+ * Download a resource from `url` to `dest`.
+ * @param {string} url - Valid URL to attempt download of resource
+ * @param {string} dest - Valid path to save the file.
+ * @returns {Promise<void>} - Returns asynchronously when successfully completed download
+ */
+function download(url, dest) {
+  return new Promise((resolve, reject) => {
+    // Check file does not exist yet before hitting network
+    fs.access(dest, fs.constants.F_OK, (err) => {
+        // if (err === null) reject('File already exists');
+
+        const request = https.get(url, response => {
+            if (response.statusCode === 200) {
+                const file = fs.createWriteStream(dest, { flags: 'w' });
+
+                file.on('finish', () => resolve());
+                file.on('error', err => {
+                        file.close();
+                        if (err.code === 'EEXIST') reject('File already exists');
+                        else fs.unlink(dest, () => reject(err.message)); // Delete temp file
+                });
+                response.pipe(file);
+                } else if (response.statusCode === 302 || response.statusCode === 301) {
+                //Recursively follow redirects, only a 200 will resolve.
+                download(response.headers.location, dest).then(() => resolve());
+            } else {
+                reject(`Server responded with ${response.statusCode}: ${response.statusMessage}`);
+            }
+          });
+      
+          request.on('error', err => {
+            reject(err.message);
+          });
+    });
+  });
+}
+
 async function main() {
-    console.log('############################################')
-    console.log('Constructing Merkle Tree..')
-    console.log('############################################')
+    // Constructing Local Merkle Tree
     let merkleTree = await constructMerkleTree()
-    // console.log(merkleTree.stdout.toString())
 
-    console.log('############################################')
-    console.log('Writing Merkle Tree to File: [merkle.json].')
-    console.log('############################################')
-    await writeMerkleTreeToStorage(merkleTree)
+    // Writing Merkle Tree to File: [merkle.json]
+    await writeMerkleTreeToStorage(merkleTree.stdout)
+    let localTree = JSON.parse(merkleTree.stdout)
 
-    console.log('############################################')
-    console.log('Flattening Client Build..')
-    console.log('############################################')
+    // Downloading Remote Merkle Tree  and Read to memory
+    let remoteMerkleTreeURL = (await getRemoteMerkleTree()).data.browser_download_url
+    await download(remoteMerkleTreeURL, `${basePath}remoteMerkle.json`)
+    let remoteTree = JSON.parse(fs.readFileSync(`${basePath}remoteMerkle.json`, "utf8"))
+
+    // Comparing Local with Remote master hashes
+    let remoteMasterHash = remoteTree.hash
+    let localMasterHash = localTree.hash
+
+    if(remoteMasterHash === localMasterHash) throw new Error('Client already synced with remote!')
+
+    let { mapping: remoteMapping, hashes: remoteHashes } = traverseTree(localTree)
+    let { mapping: localMapping, hashes: localHashes } = traverseTree(remoteTree)
+    
+    let diffs = localHashes
+    .filter(val => !remoteHashes.includes(val))
+    
+    for(let diff in diffs) {
+        let diffPath = localMapping[diffs[diff]]
+        let diffId = assetsMapping[diffPath]
+        console.log(diffPath, diffId)
+
+        const le = await updateRelease(diffId)
+        console.log(le)
+    }
+
+    
+    // flatten client build
     await flattenBuild()
+    console.log('Client flattened')
+    
 
-
-    // await createServerPackage();
-    // await moveBinaries();
+    console.log("####")
 }
 oraPromise(main(), { text: `[FC_SCRIPTS] Integrating Build...\n`, successText: '[FC_SCRIPTS] Done\n', failText: '[FC_SCRIPTS] Cannot build server'});
